05ca643fc75ef11985cadc4a0e56f8ad76d1deeb
Charles-ace <akpancharlze01@gmail.com>
2026-05-11 18:06:39 +0100

    Remove test file

--- api/test-dep.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  const { v4: uuidv4 } = require('uuid');
-  const axios = require('axios');
-  const { ethers } = require('ethers');
-  res.end(JSON.stringify({ ok: true, id: uuidv4(), hasAxios: !!axios, hasEthers: !!ethers }));
-};
