851d28d072a29590e853a9e03133a7defd9e7aab
Charles-ace <akpancharlze01@gmail.com>
2026-05-17 04:06:54 +0100

    fix: use str keys for TreeMap and add constructor param for Studio deployment

--- contracts/Orbitjob.py
+++ contracts/Orbitjob.py
@@ -4,20 +4,20 @@ from genlayer import *
 
 class Orbitjob(gl.Contract):
     task_counter: u32
-    titles: TreeMap[u32, str]
-    descriptions: TreeMap[u32, str]
-    rewards: TreeMap[u32, u32]
-    statuses: TreeMap[u32, str]
-    agents: TreeMap[u32, str]
-    outputs: TreeMap[u32, str]
+    titles: TreeMap[str, str]
+    descriptions: TreeMap[str, str]
+    rewards: TreeMap[str, str]
+    statuses: TreeMap[str, str]
+    agents: TreeMap[str, str]
+    outputs: TreeMap[str, str]
 
-    def __init__(self):
-        pass
+    def __init__(self, name: str):
+        self.task_counter = 0
 
     @gl.public.write
-    def post_task(self, title: str, description: str, reward: u32) -> u32:
-        self.task_counter += u32(1)
-        task_id = self.task_counter
+    def post_task(self, title: str, description: str, reward: str) -> None:
+        self.task_counter = self.task_counter + 1
+        task_id = str(self.task_counter)
 
         self.titles[task_id] = title
         self.descriptions[task_id] = description
@@ -26,31 +26,24 @@ class Orbitjob(gl.Contract):
         self.agents[task_id] = ""
         self.outputs[task_id] = ""
 
-        return task_id
-
     @gl.public.write
-    def submit_execution(self, task_id: u32, output: str, agent_id: str) -> bool:
-        if self.statuses[task_id] == "":
-            raise Exception("Task not found")
-
+    def submit_execution(self, task_id: str, output: str, agent_id: str) -> None:
         self.outputs[task_id] = output
         self.agents[task_id] = agent_id
         self.statuses[task_id] = "COMPLETED"
 
-        return True
-
     @gl.public.view
-    def get_task_title(self, task_id: u32) -> str:
+    def get_task_title(self, task_id: str) -> str:
         return self.titles[task_id]
 
     @gl.public.view
-    def get_task_status(self, task_id: u32) -> str:
+    def get_task_status(self, task_id: str) -> str:
         return self.statuses[task_id]
 
     @gl.public.view
-    def get_task_output(self, task_id: u32) -> str:
+    def get_task_output(self, task_id: str) -> str:
         return self.outputs[task_id]
 
     @gl.public.view
-    def get_task_counter(self) -> u32:
-        return self.task_counter
+    def get_task_count(self) -> str:
+        return str(self.task_counter)
